498623db005ce2b82dbe6fddbb08e118fe982bd7
Yash Gokakkar <yashgokakkar@gmail.com>
2024-02-22 21:48:04 +0530

    Projects section updated

--- yashg/src/components/ProjectsCard.jsx
+++ yashg/src/components/ProjectsCard.jsx
@@ -6,7 +6,7 @@ const ProjectsCard = ({ image, projectName, description, githubLink, liveLink })
   return (
     <div className="p-4">
       <div data-aos="zoom-in" className="h-fit w-[300px] md:w-[320px] border-2 border-gray-200 border-opacity-60 rounded-2xl overflow-clip lg:overflow-hidden hover:shadow-md lg:hover:scale-105 transition-all backdrop-blur-3xl shadow-xl">
-        <a href={liveLink}>
+        <a href={liveLink} target="_blank">
           <img src={image} alt={projectName} className="lg:h-48 h-[200px] w-full object-cover object-center p-2 bg-black"/>
         </a>
         <div className="p-4 flex flex-col ld:gap-3">
@@ -15,10 +15,10 @@ const ProjectsCard = ({ image, projectName, description, githubLink, liveLink })
           </div>
           <h3 className="text-lg lg:text-lg text-white">{description}</h3>
           <div className="flex items-center justify-between">
-            <a href={githubLink}>
+            <a href={githubLink} target="_blank">
               <FaGithub className="font-bold inline-flex item-center text-2xl md:text-2xl md:mb-2 lg:mb-0 hover:text-gray-800 cursor-pointer"/>
             </a>
-            <a href={liveLink}>
+            <a href={liveLink} target="_blank">
               <RiSignalTowerFill className="font-bold inline-flex item-center text-2xl md:text-2xl md:mb-2 lg:mb-0 hover:text-gray-800 cursor-pointer"/>
             </a>
           </div>
--- yashg/src/components/ProjectsSection.jsx
+++ yashg/src/components/ProjectsSection.jsx
@@ -5,6 +5,7 @@ import { useRef } from "react";
 import map2pro from '../assets/map2procoder.png';
 import mysoc from '../assets/mysoc.png';
 import netflix from '../assets/netflix.png';
+import cryptdrive from '../assets/cryptdrive.png';
 
 const ProjectsSection = () => {
   const scrollRef = useRef(null);
@@ -20,24 +21,30 @@ const ProjectsSection = () => {
       image: map2pro,
       projectName: "map2proCoder",
       description: "Developed map2proCoder website using React.js to provide comprehensive guidance and resources in Web Development, Android Development, DSA, Machine Learning, and other related fields.",
-      githubLink: "github_link1",
-      liveLink: "live_link1",
+      githubLink: "https://github.com/yashgokakkar99/map2procoder",
+      liveLink: "https://map2procoder.vercel.app/",
     },
-    {
-      image: mysoc,
-      projectName: "MySoc",
-      description: "Using this website, the “Society” ecosystem is enhanced as it brings the daily activities and transactions in online mode.",
-      githubLink: "github_link2",
-      liveLink: "live_link2",
-    },
+    // {
+    //   image: mysoc,
+    //   projectName: "MySoc",
+    //   description: "Using this website, the “Society” ecosystem is enhanced as it brings the daily activities and transactions in online mode.",
+    //   githubLink: "github_link2",
+    //   liveLink: "live_link2",
+    // },
     {
       image: netflix,
       projectName: "Netflix Clone",
       description: "Created a basic functional Netflix clone utilizing React.js and Firebase to gain hands-on experience in foundational web development skills.",
-      githubLink: "github_link3",
-      liveLink: "live_link3",
+      githubLink: "https://github.com/yashgokakkar99/netflix",
+      liveLink: "https://neflix-react-yash.web.app/",
     },
-    // Add more projects as needed
+    {
+      image: cryptdrive,
+      projectName: "Crypt Drive",
+      description: "Crypt Drive is a decentralized document storage web application. Developed using React.js, MongoDB database, IPFS storage.",
+      githubLink: "https://github.com/yashgokakkar99/BE-Project-2024",
+      liveLink: "live_link4",
+    }
   ];
 
   return (
